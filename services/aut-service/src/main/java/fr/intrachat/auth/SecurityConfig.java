86b3d3214bf4cbb4d0eefe7e1051c029ed2c6986
MichelEloka <elokamichel@gmail.com>
2025-12-28 21:49:53 +0100

    fix deploy3

--- services/aut-service/src/main/java/fr/intrachat/auth/SecurityConfig.java
+++ services/aut-service/src/main/java/fr/intrachat/auth/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
 
     return http.build();
   }
-//   salut2
+//   salut3
   @Bean
   CorsConfigurationSource corsConfigurationSource(
       @Value("${app.cors.allowed-origins:*}") String allowedOrigins) {
